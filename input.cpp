9b6651acc49c1dfe6d0dcbf137fffb60230e15ac
Kavya-Shekar <kavyay2k@gmail.com>
2021-02-18 15:46:22 +0530

    Works for classes (member variables and functions) and header files

---
+++ input.cpp
@@ -0,0 +1,63 @@
+#include <iostream.h>
+#include <x>
+#include <iostream.h>
+#include "iostream.h"
+using namespace ello;
+#include "iostream.h"
+#include <iostream>
+#include "iostream"
+#include <iostream>
+using namespace std;
+#include "iostreamx"
+#include "iostream.h"
+#include <iostream>
+#include <_ghjy>
+#include "iostream"
+#include "iostream.h"
+using namespace std;
+using namespace std;
+using namespace std;
+
+class ABC{
+	public: 
+		int yft;
+		char* kf;
+		virtual int print()
+		{
+			int h = 9;
+		}
+	private: 
+		int xgy;
+		const int j = 5;
+		static float* t;
+		static const int g = 9;
+		float Gth();
+		int print()
+		{
+			int h = 9;
+		}
+}xyz, jh, lhkg;
+
+class VGFB : public ACS, virtual private HYT{
+};
+
+
+/*
+int main() {    
+    float n1, n2, n3;
+
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+
+    if(n1 >= n2 && n1 >= n3)
+        cout << "Largest number: " << n1;
+
+    if(n2 >= n1 && n2 >= n3)
+        cout << "Largest number: " << n2;
+    
+    if(n3 >= n1 && n3 >= n2)
+        cout << "Largest number: " << n3;
+  
+    return 0;
+}*/
+
